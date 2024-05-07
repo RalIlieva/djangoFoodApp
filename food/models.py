2f9d436862603057140704bdf61290b73d4dd23d
RalIlieva <rilieva8820@gmail.com>
2024-05-07 21:30:14 +0300

    Initial commit

---
+++ food/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class Item(models.Model):
+    item_name = models.CharField(max_length=200)
+    item_desc = models.CharField(max_length=200)
+    item_price = models.IntegerField()
+
+    def __str__(self):
+        return self.item_name
+
+
---
+++ food/urls.py
@@ -0,0 +1,11 @@
+from .import views
+from django.urls import path
+
+app_name = 'food'
+urlpatterns =[
+    # /food/
+    path('', views.index, name='index'),
+    # /food/1
+    path('<int:item_id>/', views.detail, name='detail'),
+    path('item/', views.item, name='item'),
+]
