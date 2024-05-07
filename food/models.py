d3518d350b8667f4f47fa1b126120bf9e2d83349
RalIlieva <rilieva8820@gmail.com>
2024-05-07 23:14:48 +0300

    Add image field to model, makemigrations, sqlmigrate and migrate, change index.html to display images

--- food/models.py
+++ food/models.py
@@ -5,6 +5,7 @@ class Item(models.Model):
     item_name = models.CharField(max_length=200)
     item_desc = models.CharField(max_length=200)
     item_price = models.IntegerField()
+    item_image = models.CharField(max_length=550, default='https://cdn-icons-png.flaticon.com/512/1147/1147805.png')
 
     def __str__(self):
         return self.item_name
