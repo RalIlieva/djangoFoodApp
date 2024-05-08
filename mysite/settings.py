85f69d6d8849e5cfd3ccad12056b59ac67806d1e
RalIlieva <rilieva8820@gmail.com>
2024-05-08 22:20:00 +0300

    add path to upload pictures. First in admin add manually a profile and a picture = > saved in mysite/profile_pictures. Better way to organise - in mysite/settings.py add MEDIA_ROOT and MEDIA_URL - then the pictures will be saved on the server in a cleaner was - mysite/pictures/profile_pictures

--- mysite/settings.py
+++ mysite/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -126,3 +126,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = 'food:index'
 LOGIN_URL = 'login'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'pictures')
+MEDIA_URL = '/pictures/'
