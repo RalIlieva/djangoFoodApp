8a8d406e9cd76e0c57c7cb871539ad829891b7cc
RalIlieva <rilieva8820@gmail.com>
2024-05-08 18:38:03 +0300

    Add additonal field in the form - UserCreationForm - with form.py and inherit from the class - replace then the intial class with the newely created RegisterForm

--- users/views.py
+++ users/views.py
@@ -1,18 +1,19 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+# from django.contrib.auth.forms import UserCreationForm - initial version - extended the form with RegisterForm
 from django.contrib import messages
+from .forms import RegisterForm
 
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = RegisterForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Welcome {username}, your account is created!')
             return redirect('food:index')
     else:
-        form = UserCreationForm()
+        form = RegisterForm()
     return render(request, 'users/register.html', {'form': form})
 
 
