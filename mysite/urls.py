366643ee08cfc0ac5c8d1507fab03902cbb9b3ac
RalIlieva <rilieva8820@gmail.com>
2024-05-13 20:38:11 +0300

    Change the url to profile/username so it can be used as a hyperlink later. Change the users/views.py, register in mysite/urls.py and add in the food/base.html the link in the navbar - url 'profile' username=request.user.username

--- mysite/urls.py
+++ mysite/urls.py
@@ -28,7 +28,7 @@ urlpatterns = [
     path('register/', user_views.register, name='register'),
     path('login/', authentication_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', authentication_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
-    path('profile/', user_views.profilepage, name='profile'),
+    path('profile/<str:username>/', user_views.profilepage, name='profile'),
 ]
 
 # Copy this code from documentation - how to display images in development serve
