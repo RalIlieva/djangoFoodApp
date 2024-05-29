d2df0ede432c08efe2cc92abd2276703da99f199
RalIlieva <rilieva8820@gmail.com>
2024-05-29 21:59:55 +0300

    Add api/views.py

--- api/views.py
+++ api/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from food.models import Item, Comment
+from users.models import Profile
+from django.contrib.auth.models import User
+from .serializers import ItemSerializer, CommentSerializer, ProfileSerializer, UserSerializer
 
 # Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class ProfileViewSet(viewsets.ModelViewSet):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+
+class ItemViewSet(viewsets.ModelViewSet):
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
