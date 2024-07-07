29424f1d9e169f57b68ea770c3e39e3c38fbb864
RalIlieva <rilieva8820@gmail.com>
2024-07-07 15:46:31 +0300

    Change users/model to use uuid unique identifier for saving profile images

--- users/models.py
+++ users/models.py
@@ -1,13 +1,20 @@
+import uuid
+import os
 from django.db import models
 from django.contrib.auth.models import User
 
 
+def upload_to(instance, filename):
+    """Generate file path for new user image."""
+    ext = filename.split('.')[-1]
+    filename = f'{uuid.uuid4()}.{ext}'
+    return os.path.join('profile_pictures', filename)
+
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default='profilepic.jpg', upload_to='profile_pictures')
+    image = models.ImageField(default='profilepic.jpg', upload_to=upload_to)
     location = models.CharField(max_length=100)
 
     def __str__(self):
         return self.user.username
-
-
