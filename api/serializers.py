01ed7022001bb430c65d4818e82e90aa1bad4b37
RalIlieva <rilieva8820@gmail.com>
2024-07-15 23:20:33 +0300

    API serializers - overwrite vailidate method in MyTokenObtainPairSerializer to customize with username and email in addition to refresh and access in the response

--- api/serializers.py
+++ api/serializers.py
@@ -8,13 +8,20 @@ from rest_framework_simplejwt.views import TokenObtainPairView
 
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
-    @classmethod
-    def get_token(cls, user):
-        token = super().get_token(user)
-        token['name'] = user.username
-        token['email'] = user.email
+    # @classmethod
+    # def get_token(cls, user):
+    #     token = super().get_token(user)
+    #     token['name'] = user.username
+    #     token['email'] = user.email
+    #
+    #     return token
+    def validate(self, attrs):
+        data = super().validate(attrs)
 
-        return token
+        data['username'] = self.user.username
+        data['email'] = self.user.email
+
+        return data
 
 
 class UserSerializer(serializers.ModelSerializer):
